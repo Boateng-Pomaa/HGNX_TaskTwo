f26561936a6adc187f64cc761c20864abcf27268
Boateng Pomaa Linda <lindapomaa27@gmail.com>
2023-09-09 17:57:16 +0100

    first commit

---
+++ taskTwo.js
@@ -0,0 +1,44 @@
+import express from "express"
+import cors from 'cors'
+import moment from 'moment'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+
+app.get('/task', async (req, res) => {
+    try {
+        const { slack_name, track } = req.query
+        const currentDay = moment().utc().format('dddd')
+        const utcTime = moment().utc().add(2, 'minutes').format('YYYY-MM-DDTHH:mm:ss') + 'Z'
+        const FileUrl = 'https://github.com/username/repo/blob/main/file_name.ext'
+        const RepoUrl = 'https://github.com/username/repo'
+        const statusCode = 200
+
+        const response = {
+            slack_name: slack_name,
+            current_day: currentDay,
+            utc_time: utcTime,
+            track: track,
+            github_file_url: FileUrl,
+            github_repo_url: RepoUrl,
+            status_code: statusCode
+        };
+
+        res.json(response)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send('Internal Server Error')
+    }
+}
+
+)
+
+const port = 5000
+app.listen(port, () => {
+    console.log(`server is listening on ${port}`)
+})
